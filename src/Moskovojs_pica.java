ce989f15c150ab72b32c14af6ab17ee706364515
Moskovojs <rihards.moskovojs05@gmail.com>
2023-03-22 14:36:44 +0200

    Izveidoju iespeju gan nopirkt gan pasutit picu un aprekinat summu

--- src/Moskovojs_pica.java
+++ src/Moskovojs_pica.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 public class Moskovojs_pica {
@@ -5,6 +9,19 @@ public class Moskovojs_pica {
 	public static void main(String[] args) {
 		String izvele;
 		String darbibas[] = {"Nopirkt picu", "Pasutit picu", "Aiziet", "Apturet"};
+		
+		int izmersCena = 0;
+		int merceCena = 0;
+		int piedavasCena = 0;
+		int summa = 0;
+		String merces[] = {"Kecupa", "Kiploku", "Pesto"};
+		String izmers = "";
+		int izmeri;
+		String merce = "";
+		String mercee[] = {};
+		String piedavas = "";
+		String piedavasM[] = {};
+		
 		boolean majas = false;
 		
 		JOptionPane.showMessageDialog(null, "Sveicinati labakaja picerija", null, JOptionPane.INFORMATION_MESSAGE);
@@ -13,16 +30,70 @@ public class Moskovojs_pica {
 			switch(izvele){
 			case "Nopirkt picu":
 				if(majas==false) {
-				String izmers =(String) JOptionPane.showInputDialog(null);
-				String merce = (String)JOptionPane.showInputDialog(null);
-				String piedavas = (String)JOptionPane.showInputDialog(null);
-				
+				 izmeri = JOptionPane.showOptionDialog(null, "Cik lielu picu velies?", null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+						 null, new String[] {"20cm", "30cm", "50cm"}, null);
+				 switch(izmeri){
+		   			case 0:
+		   				izmers = "20cm";
+		                izmersCena = 5;
+		                break;
+		            case 1:
+		                izmers = "30cm";
+		                izmersCena = 6;
+		                break;
+		            case 2:
+		                izmers = "50cm";
+		                izmersCena = 8;
+		                break;
+		        }
+				 merce = (String)JOptionPane.showInputDialog(null, "Kadu merci velies?", null, JOptionPane.QUESTION_MESSAGE, null, merces, merces[0]);
+				 List<String> merceM = new ArrayList<String>(Arrays.asList(mercee));
+				 merceM.add(merce);
+				 
+				 piedavas = (String)JOptionPane.showInputDialog(null, "Kadas piedevas picai?(Ar komatu atdali)", null, JOptionPane.QUESTION_MESSAGE);
+			     piedavasM = piedavas.split(",");
+			      
+			     merceCena = merceM.size()*2;
+			     piedavasCena = piedavasM.length * 1;
+			     summa = izmersCena + merceCena + piedavasCena;
+			        
+			     Rihards_pica pica = new Rihards_pica(izmers, merce, piedavas, summa);
+				 JOptionPane.showMessageDialog(null, "Pica nopirkta", null, JOptionPane.INFORMATION_MESSAGE);
+				 
 				}else	JOptionPane.showMessageDialog(null, "Jus neesiet uz vietas picerija, pameginiet pasutit picu", "error", JOptionPane.ERROR_MESSAGE);
 				break;
 				
 			case "Pasutit picu":
 				if(majas==true){
-					
+					 izmeri = JOptionPane.showOptionDialog(null, "Cik lielu picu velies?", null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+							 null, new String[] {"20cm", "30cm", "50cm"}, null);
+					 switch(izmeri){
+			   			case 0:
+			   				izmers = "20cm";
+			                izmersCena = 5;
+			                break;
+			            case 1:
+			                izmers = "30cm";
+			                izmersCena = 6;
+			                break;
+			            case 2:
+			                izmers = "50cm";
+			                izmersCena = 8;
+			                break;
+			        }
+					 merce = (String)JOptionPane.showInputDialog(null, "Kadu merci velies?", null, JOptionPane.QUESTION_MESSAGE, null, merces, merces[0]);
+					 List<String> merceM = new ArrayList<String>(Arrays.asList(mercee));
+					 merceM.add(merce);
+					 
+					 piedavas = (String)JOptionPane.showInputDialog(null, "Kadas piedevas picai?(Ar komatu atdali)", null, JOptionPane.QUESTION_MESSAGE);
+				     piedavasM = piedavas.split(",");
+				      
+				     merceCena = merceM.size()*2;
+				     piedavasCena = piedavasM.length * 1;
+				     summa = izmersCena + merceCena + piedavasCena;
+				        
+				     Rihards_pica pica = new Rihards_pica(izmers, merce, piedavas, summa);
+					 JOptionPane.showMessageDialog(null, "Pica pasutita", null, JOptionPane.INFORMATION_MESSAGE);	
 				}else 	JOptionPane.showMessageDialog(null, "Jus neesiet majas, pameginiet nopirkt picu", "error", JOptionPane.ERROR_MESSAGE);
 
 				break;
